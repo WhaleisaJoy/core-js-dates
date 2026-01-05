7e423ee3c267d36a582cb50314d467b0a170143d
Whale Joy <afterlifelineonomega@gmail.com>
2026-01-05 14:34:10 +0300

    feat: implement getNextFriday task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -73,8 +73,16 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const nextFriday = new Date(date);
+
+  let daysUntilFriday = 5 - nextFriday.getDay();
+  if (daysUntilFriday <= 0) {
+    daysUntilFriday += 7;
+  }
+
+  nextFriday.setDate(nextFriday.getDate() + daysUntilFriday);
+  return nextFriday;
 }
 
 /**
